a475a1fac89b44242612fa81915d8d50c16ff912
ByLeo-native <heyleoqueonda1@gmail.com>
2022-08-23 16:35:15 -0300

    Primer commit- Creacion del proyecto Java

---
+++ Proyecto/src/launcher/Launcher.java
@@ -0,0 +1,15 @@
+package launcher;
+
+import gui.SimplePresentationScreen;
+
+public class Launcher {
+	public static void main(String [] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	SimplePresentationScreen presentacion = new SimplePresentationScreen();
+            }
+        });
+    }
+}
